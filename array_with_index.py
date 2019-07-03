0e8e84028883457bf8d627039ee0e85a951d508c
kirumalai <51952655+kirumalai@users.noreply.github.com>
2019-07-03 17:11:52 +0530

    Update array_with_index.py

--- array_with_index.py
+++ array_with_index.py
@@ -1,4 +1,4 @@
-input=int(input())
-makelist=list(map(int,input().split()))
-for i in range(input):
-  print(makelist[i],i)
+abc=int(input(""))
+arr12=list(map(int,input().split()))
+for e in range(abc):
+  print(arr12[e],e)
