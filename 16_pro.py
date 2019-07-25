5165fc0d1493c2faacf2a5bb0d617808d96f405e
krithika <51952655+kirumalai@users.noreply.github.com>
2019-07-25 22:06:37 +0530

    Update 16_pro.py

--- 16_pro.py
+++ 16_pro.py
@@ -3,7 +3,7 @@ lis1=list(map(int,input().split()))
 at=[1]*numb1
 for km in range(numb1):
     if(km==0):
-        if(lis1[dt]>lis1[dt+1]):
+        if(lis1[km]>lis1[km+1]):
             at[km]=at[km]+at[km+1]
     elif(km>0):
         if(lis1[km]>lis1[km-1]):
