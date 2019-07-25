e9532fa53d43bd5f424b32afdc69be7aca228645
krithika <51952655+kirumalai@users.noreply.github.com>
2019-07-25 22:04:03 +0530

    Create 16_pro.py

---
+++ 16_pro.py
@@ -0,0 +1,11 @@
+numb1=int(input())
+lis1=list(map(int,input().split()))
+at=[1]*numb1
+for km in range(numb1):
+    if(km==0):
+        if(lis1[dt]>lis1[dt+1]):
+            at[km]=at[km]+at[km+1]
+    elif(km>0):
+        if(lis1[km]>lis1[km-1]):
+            at[km]=at[km]+at[km-1]
+print(sum(at))
