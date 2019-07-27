1a8a01b6dd15c5086fafff5ac204f8261679c77f
krithika <51952655+kirumalai@users.noreply.github.com>
2019-07-27 15:43:23 +0530

    Update 26_pro.py

--- 26_pro.py
+++ 26_pro.py
@@ -4,7 +4,7 @@ def sub(rt):
     for i in range (1 , miit): 
         for j in range(0 , i): 
             if rt[i] > rt[j] and subbb[i]< subbb[j] + 1 : 
-                subbb[i] = sub[j]+1
+                subbb[i] = subbb[j]+1
     maximum = 0
     for i in range(miit): 
         maximum = max(maximum , subbb[i])  
