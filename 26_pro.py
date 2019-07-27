cb42be68905304c07a684097e22744c7707d49f6
krithika <51952655+kirumalai@users.noreply.github.com>
2019-07-27 15:42:09 +0530

    Create 26_pro.py

---
+++ 26_pro.py
@@ -0,0 +1,14 @@
+def sub(rt): 
+    miit = len(rt) 
+    subbb = [1]*miit 
+    for i in range (1 , miit): 
+        for j in range(0 , i): 
+            if rt[i] > rt[j] and subbb[i]< subbb[j] + 1 : 
+                subbb[i] = sub[j]+1
+    maximum = 0
+    for i in range(miit): 
+        maximum = max(maximum , subbb[i])  
+    return maximum
+ar1=int(input()) 
+rt = list(map(int,input().split()))
+print (subbb(rt))
